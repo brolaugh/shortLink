d708ee33e284139410557e9ba4d2963c871dc106
Brolaugh <haki1003@gmail.com>
2015-08-20 00:44:42 +0200

    Added baseline for the simple frontpage
    Signed-off-by: Brolaugh <haki1003@gmail.com>

---
+++ index.php
@@ -0,0 +1,34 @@
+<html lang="en">
+  <head>
+    <title>Test123</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Head</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3"/>
+        <div class="col-md-6 ">
+        <form action="" method="post">
+          <label for="long_url">Url to be shortened</label>
+          <input name="long_url" autofocus="true" >
+          <button/>
+        </form>
+      </div>
+      <div class="col-md-3"/>
+      </div>
+    </div>
+
+  </body>
+</html>
+
+<?php
+
+ ?>
